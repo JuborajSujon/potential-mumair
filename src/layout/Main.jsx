8dd64bd7ba4db45c11b14f6301e827194b41d079
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-10 23:25:58 +0600

    fix theme controller button possition and state value presist

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import ThemeController from "../components/ThemeController";
 
 const Main = () => {
   return (
-    <div className="font-poppins max-w-[1420px] mx-auto">
-      <h3>Navbar</h3>
-      <div className="dark:bg-slate-800 min-h-[calc(100vh-110px)] overflow-x-hidden">
-        <Outlet />
-        <h2>ThemeController</h2>
+    <div className="font-poppins dark:bg-[#1e1e1e]">
+      <div className="max-w-[1420px] mx-auto relative">
+        <h3>Navbar</h3>
+        <div className="min-h-[calc(100vh-110px)] overflow-x-hidden">
+          <Outlet />
+          <ThemeController />
+        </div>
+        <h3>Footer</h3>
       </div>
-      <h3>Footer</h3>
     </div>
   );
 };
